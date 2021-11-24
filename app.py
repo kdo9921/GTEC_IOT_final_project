3ab65170b4509c660eabc6e4d2ef5626ec23e831
kiwi <kiwi@darae.dev>
2021-11-24 14:37:12 +0900

    팬 버튼 추가

--- app.py
+++ app.py
@@ -1,36 +1,56 @@
 import time
 import aiy.device._led as LED
+import aiy.device._fan as FAN
 import RPi.GPIO as GPIO
+import aiy.device._dht11 as DHT
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-led = 'off'
+data = {'led':'off', 'temp':0, 'fan':'off'}
+
+def initGPIO():
+    GPIO.setmode(GPIO.BCM)
+    LED.initLedModule()
+    FAN.initFan(FAN.FAN_PIN1,FAN.FAN_PIN2)
+
+def getTemp():
+    global data
+    data['temp'] = DHT.readTemp()
 
 @app.route('/',methods=('GET', 'POST'))
 def index():
-    global led
-    return render_template('index.html', isLed=led)
+    global data
+    return render_template('index.html', data=data)
 
 @app.route('/api/led',methods=['POST'])
 def ledToggle():
-    global led
-    print("LED 값 : " + led)
-    if led == 'on':
-        led = 'off'
+    global data
+    if data['led'] == 'on':
+        data['led'] = 'off'
         LED.controlLed(LED.LED_PIN1, LED.OFF)
         LED.controlLed(LED.LED_PIN2, LED.OFF)
         LED.controlLed(LED.LED_PIN3, LED.OFF)
         LED.controlLed(LED.LED_PIN4, LED.OFF)
-    elif led == 'off':
-        led = 'on'
+    elif data['led'] == 'off':
+        data['led'] = 'on'
         LED.controlLed(LED.LED_PIN1, LED.ON)
         LED.controlLed(LED.LED_PIN2, LED.ON)
         LED.controlLed(LED.LED_PIN3, LED.ON)
         LED.controlLed(LED.LED_PIN4, LED.ON)
-    print("led 현재 상태 : " + led)
-    return render_template('index.html', isLed=led)
+    return render_template('index.html', data=data)
+
+@app.route('/api/fan',methods=['POST'])
+def fanToggle():
+    global data
+    if data['fan'] == 'on':
+        data['fan'] = 'off'
+        FAN.controlFan(FAN.OFF)
+    elif data['fan'] == 'off':
+        data['fan'] = 'on'
+        FAN.controlFan(FAN.ON)
+    return render_template('index.html', data=data)
 
 if __name__=="__main__":
-    GPIO.setmode(GPIO.BCM)
-    LED.initLedModule()
+    initGPIO()
+    getTemp()
     app.run(host="0.0.0.0", port="5000",debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -13,6 +13,25 @@ var led = function() {
     form.submit();	
 };
 document.addEventListener("DOMContentLoaded", function() {
-    var startbtn = document.getElementById("btn_test");
+    var startbtn = document.getElementById("btn_led");
     startbtn.addEventListener("click",led);
+});
+
+var fan = function() {
+    var currentLed = document.getElementById("fan").textContent;
+    var form = document.createElement('form');
+    form.setAttribute('method', 'post'); 
+    form.setAttribute('action', '/api/fan');	
+    form.setAttribute('target', 'iframe1');	
+    document.charset = "utf-8";
+        var hiddenField1 = document.createElement('input');
+        hiddenField1.setAttribute('type', 'hidden'); 
+        hiddenField1.setAttribute('name', "fan");
+        hiddenField1.setAttribute('value', currentLed);
+    document.body.appendChild(form);
+    form.submit();	
+};
+document.addEventListener("DOMContentLoaded", function() {
+    var startbtn = document.getElementById("btn_fan");
+    startbtn.addEventListener("click",fan);
 });
